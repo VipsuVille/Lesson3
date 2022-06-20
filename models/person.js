60c09a6f025e9174cb647d6560e3280c50b75bf6
VipsuVille <vipsu@protonmail.com>
2022-06-20 20:21:32 +0300

    18.6

---
+++ models/person.js
@@ -0,0 +1,52 @@
+const mongoose = require('mongoose')
+
+const url = process.env.MONGODB_URI
+
+console.log('connecting to', url)
+mongoose.connect(url)
+  .then(result => {
+    console.log('connected to MongoDB')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
+
+const noteSchema = new mongoose.Schema({
+  content: {
+    type: String,
+    minlength: 3,
+    required: true
+  },
+  date: { 
+    type: Date,
+    required: true
+  },
+  number: {
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function(v) {
+        return /\d{3}-\d{5}/.test(v);
+      }
+  }},
+  contentNum: {
+    type: String,
+    minlength: 8,
+    validate: {
+      validator: function(v) {
+        return /\d{3}-\d{5}/.test(v);
+      }
+  }
+  },
+  important: Boolean,
+})
+
+noteSchema.set('toJSON', {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
+
+module.exports = mongoose.model('Note', noteSchema)
